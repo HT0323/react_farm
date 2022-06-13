88b360ccec59f0154cc7ee9935c3dcf3da301c96
HaraT0323 <alra9si916323@gmail.com>
2022-06-14 01:32:02 +0900

    ユーザー,タスク一覧情報のキャッシュ化

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,8 +1,13 @@
 import { LogoutIcon } from "@heroicons/react/outline";
 import { useProcessAuth } from "../hooks/useProcessAuth";
+import { useQueryTasks } from "../hooks/useQueryTasks";
+import { useQueryUser } from "../hooks/useQueryUser";
 
 export const Todo = () => {
   const { logout } = useProcessAuth();
+  const { data: dataUser } = useQueryUser();
+  const { data: dataTasks, isLoading:  isLoadingTasks } = useQueryTasks();
+
   return (
     <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono">
       <LogoutIcon
